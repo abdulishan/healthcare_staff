a4ff3ae125a7c6f968870e3e6532fb475f9bea85
Abdul Ishan <abdulishankhan5@gmil.com>
2025-07-05 17:48:36 +0530

    code changes

--- frontend/src/app/component/public/public.component.ts
+++ frontend/src/app/component/public/public.component.ts
@@ -1,11 +1,29 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-public',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './public.component.html',
   styleUrl: './public.component.scss'
 })
 export class PublicComponent {
+  healthReports:any = [
+    {
+      id:1,
+      illnessName: "Covid-19 update",
+      description: "Stay inform about the latest covid 19 guidlines."
+    },
+    {
+      id:2,
+      illness: "Seasonal Flu Infection",
+      description: "Learn about steps you can take to prevent flu."
+    },
+    {
+      id:3,
+      illness: "Mental health Awareness",
+      description: "Explore resource and supports to maintain good mental health."
+    },
 
+  ]
 }
